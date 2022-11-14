301b826ba1c60d796a64fda55eaac21afc60d7ee
JerryYangJ <113914359+JerryYangJ@users.noreply.github.com>
2022-11-14 13:31:13 +0800

    Create QCheck控件.py

---
+++ QCheck控件.py
@@ -0,0 +1,41 @@
+# @Time :2022/10/28 11:04
+# @Author : Jerry Y
+# @File  : QCheckBox控件.py
+# @Info  : 单选
+
+
+'''
+信号：
+    stateChanged()
+
+'''
+
+# -*- coding: utf-8 -*-
+"""单选框示例"""
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+
+class CheckBox(QtWidgets.QWidget):
+    def __init__(self):
+        super(CheckBox, self).__init__()
+
+        self.setWindowTitle("单选框演示程序")
+        self.setGeometry(300, 300, 250, 150)
+
+        self.check_box = QtWidgets.QCheckBox("显示标题", self)
+        self.check_box.move(10, 10)
+        self.check_box.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.check_box.toggle()
+        self.check_box.stateChanged.connect(self.change_title)
+
+    def change_title(self):
+        if self.check_box.isChecked():
+            self.setWindowTitle("单选框演示程序")
+        else:
+            self.setWindowTitle("单选框未选中")
+
+app = QtWidgets.QApplication(sys.argv)
+cb = CheckBox()
+cb.show()
+sys.exit(app.exec_())
